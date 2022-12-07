f5a998e2d8b0092b3295e6e684c0efa7a2906ecd
edgars <edzhuks@gmail.com>
2022-12-07 22:43:07 +0200

    buy beer at <products_node>/api/buy. Format is [{"id":1,"quantity":2},{"id":2,"quantity":3}...]

--- products/routers/productRouter.js
+++ products/routers/productRouter.js
@@ -13,4 +13,24 @@ router.get("/api/products", async function (req,res, next) {
   res.json(products);
 })
 
+router.post("/api/buy", async function (req, res, next) {
+  const  items  = req.body;
+  sum = 0
+  for ({id,quantity} of items){
+    boughtquantity = quantity
+    const [{price, q},] = await db_interface.query(
+        `SELECT price, quantity as q from products WHERE id = ?`, [id]
+    );
+    if (q < boughtquantity){
+      res.json("Error: buying more than available")
+      return
+    }
+    sum += price*boughtquantity
+    db_interface.query(
+        `UPDATE products SET quantity = ? where id = ?`, [q-boughtquantity, id]
+    )
+  }
+  res.json(sum);
+})
+
 module.exports = router;
